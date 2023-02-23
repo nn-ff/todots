b9f5565c5a8b6c0086644c79d60284d836dae4c8
nn-ff <ilyakom3@gmail.com>
2023-02-24 01:00:59 +0500

    edit focus

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -12,6 +12,7 @@ const TodoItem: React.FC<TodoItemProps> = ({todo, index}) => {
   const [editMode, setEditMode] = React.useState<boolean>(false)
   const [value, setValue] = React.useState<string>(todo.body)
   const dispatch = useAppDispatch()
+  const tareaRef = React.useRef<HTMLTextAreaElement>(null)
   const inputHandler = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setValue(e.target.value)
   }
@@ -27,6 +28,16 @@ const TodoItem: React.FC<TodoItemProps> = ({todo, index}) => {
   const onCLickComplete = (id: number) => {
     dispatch(todoComplete(id))
   }
+  const onClickEdit = () => {
+      setEditMode(true)
+      setTimeout(() => {
+        console.log(tareaRef)
+      tareaRef.current?.focus()
+      tareaRef.current?.setSelectionRange(value.length, value.length)
+    }, 50);
+
+    
+  }
   return (
     <div className={`md:flex space-x-5 m-5 border border-blue-300 justify-between ${todo.isComplete ? 'bg-green-300' : 'bg-blue-200'} `}>
         <div className='w-5/6 flex md:text-2xl'>
@@ -35,7 +46,7 @@ const TodoItem: React.FC<TodoItemProps> = ({todo, index}) => {
           </div>
           {editMode 
            ?
-           <textarea  className='grow w-full p-2 m-5 self-start rounded-3xl resize-none' onChange={inputHandler} value={value}></textarea>      
+           <textarea ref={tareaRef} className='grow w-full p-2 m-5 self-start rounded-3xl resize-none' onChange={inputHandler} value={value}></textarea>      
            :
            <div className='grow m-5'>{value}.</div>}
            </div>
@@ -47,7 +58,7 @@ const TodoItem: React.FC<TodoItemProps> = ({todo, index}) => {
         <>
         <button onClick={() => onClickRemove(todo.id)} className='border border-red-800 p-1 bg-red-300'>remove</button>
         <button onClick={() => onCLickComplete(todo.id)} className={`border border-green-800 mx-2 md:mx-0 md:my-1 p-1  ${todo.isComplete ? 'bg-slate-300' : 'bg-green-400'}`}>{todo.isComplete ? 'Undo' : 'Done'}</button>
-        <button onClick={() => setEditMode(true)} className='border border-gray-800 p-1 bg-gray-300'>edit</button>
+        <button onClick={onClickEdit} className='border border-gray-800 p-1 bg-gray-300'>edit</button>
         </>}
         </div>
       </div>
